4985fa065a50ff09431725bdd64dc25e15c98aea
pprattsj <pprattsj@gmail.com>
2018-04-12 12:39:02 -1000

    Update CreateTable.sql

--- CreateTable.sql
+++ CreateTable.sql
@@ -1,17 +1,21 @@
+Create database pigtracker;
 
-Create database 'pigtracker';
+CONNECT pigtracker;
 
-
-CREATE TABLE 'PigTracker' (
-  'Collar_ID` varchar(10) COLLATE utf8_unicode_ci NOT NULL,
-                             `Timestamp` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-                             `Lat` float(14,9) NOT NULL,
-                             `Lng` float(14,9) NOT NULL
-						)
+CREATE TABLE pigtracker (
+  Collar_ID varchar(10) COLLATE utf8_unicode_ci NOT NULL,
+  
+                             Timestamp datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  
+                             Lat float(14,9) NOT NULL,
+  
+                             Lng float(14,9) NOT NULL
+)
  ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
-ALTER TABLE `PigTracker`
-  ADD KEY `CollarTime` (`Timestamp`);
+
+
+CREATE UNIQUE INDEX Collar_Time on pigtracker (collar_id,Timestamp);
 
 
 COMMIT;
